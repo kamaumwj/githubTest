05bd2f1a249acb9e3fd21f1706935ebd249e9773
John <Mwangkamauj@yahoo.com>
2022-11-18 19:10:27 +0300

    introduced animal class

---
+++ lab2.py
@@ -0,0 +1,34 @@
+class Animal:
+
+    def __init__(self, AnimalType):
+        self._type = AnimalType.lower()
+        self._sound = ""
+
+    # a getter funcion
+    @property
+    def type(self):
+        return self._type
+
+    # a setter function
+    @type.setter
+    def type(self, value):
+        self._type = value
+
+    def makenoise(self):
+        if self._type == "herbivore":
+            self._sound = "moo"
+        elif self._type == "carnivore":
+            self._sound = "Miawwwww"
+        elif self._type == "bird":
+            self._sound = "chip chip chip"
+        return self._sound
+
+
+an1 = Animal("Carnivore")
+
+print(f'The animal type is {an1.type}')
+
+## Change animal type
+an1.type = "Herbivore"
+
+print(f'The animal type is {an1.type}')
---
+++ lab3.py
@@ -0,0 +1,10 @@
+from lab2 import Animal
+
+anx = Animal("Bird")
+print(anx.type)
+print(anx.makenoise())
+
+name = 'HELLO'
+
+for char in name:
+    print(char)
