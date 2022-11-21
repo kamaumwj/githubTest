a8e66ece0ad5f36c04417da12cb68b1dc5316364
John <Mwangkamauj@yahoo.com>
2022-11-21 08:06:36 +0300

    Added new files

---
+++ lab1.py
@@ -0,0 +1,5 @@
+def greeting(first_name=""):
+    print(f'How are you  {first_name}')
+
+
+greeting("john Mwangi")
---
+++ lab3.py
@@ -0,0 +1,14 @@
+from lab2 import Animal
+
+anx = Animal("Bird")
+print(anx.type)
+print(anx.makenoise())
+
+name = 'HELLO'
+
+for char in name:
+    print(char)
+
+
+print("This is new design with commit")
+print("with small-error-fix")
